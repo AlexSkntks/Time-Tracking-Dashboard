947ac2d51f848627279a4c0306ab5a719a48e918
Alex Oliveira <alexskntks@outlook.com>
2022-01-06 11:15:39 -0300

    update

--- index.js
+++ index.js
@@ -3,11 +3,40 @@ let app = express();
 let port = 3000;
 const path  = require("path");
 
+/**
+ * Trabalhando com JSON
+ */
+
+const items = [
+	{
+		title: "A",
+		message: "Alguma coisa"
+	},
+	{
+		title: "B",
+		message: "Borboleta"
+	},
+	{
+		title: "C",
+		message: "Cachorro"
+	},
+	{
+		title: "D",
+		message: "Dinossauro"
+	}
+];
+
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
-	res.render("pages/index");
-})
+	res.render("pages/index", {
+		messages: items
+	});
+});
+
+app.get('/sobre', (req, res) =>{
+	res.render("pages/sobre");
+});
 
 app.use(express.static(path.join(__dirname,"public")))
 
