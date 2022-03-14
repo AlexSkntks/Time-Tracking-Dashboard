9d95b15c2d72b892721044c9b2a0a79b5c5d330f
Alex Oliveira <alexskntks@outlook.com>
2022-03-14 12:56:33 -0300

    teste

---
+++ api/teste.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const hello = express.Router();
+
+//A variável data contém o array de objetos de data.json
+const data = require("../public/json/data.json");
+
+hello.get("/", async (req, res) => {
+	res.json({message: "Hello world"});
+});
+
+module.exports = hello;
--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 let express = require('express');
 
 let product = require('./api/resource');
+let hello = require('./api/teste');
 
 let app = express();
 
 const path  = require("path");
 
+app.use(express.json());
 app.use(express.static(path.join(__dirname,"public")));
 
 app.set("view engine", "ejs");
 
 app.use(product);
+app.use(hello);
 
 app.listen(3000, () => {
 	console.log("Server is running at port 3000");
