d2af3671ec0c76810d1f3576f88872f348f5440d
Alex Oliveira <alexskntks@outlook.com>
2022-03-15 15:41:21 -0300

    Update resource.js

--- api/resource.js
+++ api/resource.js
@@ -2,16 +2,17 @@ const express = require('express');
 const ejs = require('ejs');
 const router = express.Router();
 
-//A variável data contém o array de objetos de data.json
-const path  = require("path");
-router.use(express.static(path.join(__dirname,"views")));
-
-const data = require(__dirname + "/../public/json/data.json");
+var data = require(__dirname + '/../public/json/data.json');
 
 router.get("/", async (req, res) => {
-	res.render("pages/index", {
-		data: data
-	});
+	try {
+		res.render(__dirname + "/../views/pages/index", {
+			data: data
+		});
+	} catch (error) {
+		res.status(500);	
+	}
+	
 });
 
 module.exports = router;
